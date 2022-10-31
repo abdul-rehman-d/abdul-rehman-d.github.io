06cc677ed9e47bf3f21ee7d40cfad597bcd7dc30
abdul-rehman-d <ar618211@gmail.com>
2022-10-31 23:57:19 +0500

    stars fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 function Navbar() {
   return (
-    <nav className="w-100 bg-transparent text-white flex flex-row justify-between items-center py-4 lg:px-32 md:px-16 px-8">
+    <nav className="w-100 bg-dark text-white flex flex-row justify-between items-center py-4 lg:px-32 md:px-16 px-8">
       <h1 className="text-2xl font-semibold">ard.</h1>
       <ul className="flex flex-row gap-4">
         <li className="cursor-pointer hover:text-primary-400">Home</li>
--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -4,19 +4,21 @@ import star from './../../assets/star.png'
 const HomeSection = () => {
   const containerRef = useRef(null)
   return (
-    <div className='fill-remaining flex items-center lg:px-32 md:px-16 px-8 md:gap-10 relative' ref={containerRef}>
-      {Array(50).fill(0).map(() => (
-        <img
-          src={star}
-          className='absolute w-10 h-10 z-0'
-          style={{
-            top: Math.floor(Math.random()*window.innerHeight-100/10),
-            left: Math.floor(Math.random()*window.innerWidth-100/10),
-            rotate: `${Math.floor(Math.random()*90-1)}deg`,
-          }}
-        />
-      ))}
-      <div className="flex flex-col gap-2 text-center md:text-left bg-dark z-10">
+    <div className='fill-remaining flex items-center lg:px-32 md:px-16 px-8 md:gap-10 relative overflow-hidden' ref={containerRef}>
+      <div>
+        {Array(window.innerWidth < 779 ? 20 : window.innerWidth > 991 ? 35 : 50).fill(0).map(() => (
+          <img
+            src={star}
+            className='absolute w-10 h-10 z-0 animate-pulse'
+            style={{
+              top: Math.floor(Math.random()*window.innerHeight-400/10),
+              left: Math.floor(Math.random()*window.innerWidth-400/10),
+              rotate: `${Math.floor(Math.random()*90-1)}deg`,
+            }}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col p-2 gap-2 text-center md:text-left bg-dark z-10">
         <p className="text-white text-3xl">Hello, I am</p>
         <h1 className="text-primary-400 text-5xl">Abdul Rehman Daniyal</h1>
         <h1 className="text-primary-600 text-3xl">A Frontend Developer</h1>
