263189dfecb6a490e85169016f8d54322575c8ac
Abdul Rehman <ar618211@gmail.com>
2023-05-10 21:00:39 +0500

    navbar on right side

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,29 @@
-import { faBars, faBriefcase, faClose, faHome, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faBriefcase, faHome, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 import { NavLink } from "react-router-dom"
-import useSidebarStore from "../store";
 
 function Navbar({ scrollTo } : { scrollTo: (hash: string) => void }) {
 
   function onClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    e.preventDefault()
     const hash = e.currentTarget.hash
     scrollTo(hash.replace('#', ''))
   }
 
   return (
-    <nav className="w-100 bg-dark text-white flex flex-row justify-between items-center py-4 lg:px-32 md:px-16 px-8 relative z-10">
-      <ul className="flex flex-row gap-4">
-        <li className="cursor-pointer hover:text-primary-400">
+    <nav className="sticky top-0 h-screen w-10 bg-dark text-white flex flex-col justify-center items-center px-8 z-10">
+      <ul className="flex flex-col gap-4 text-3xl">
+        <li className="cursor-pointer flex justify-center hover:text-primary-400">
           <NavLink to={{hash: '#home'}} onClick={onClick}>
             <FontAwesomeIcon icon={faHome} />
           </NavLink>
         </li>
-        <li className="cursor-pointer hover:text-primary-400">
+        <li className="cursor-pointer flex justify-center hover:text-primary-400">
           <NavLink to={{hash: '#about'}} onClick={onClick}>
             <FontAwesomeIcon icon={faUser} />
           </NavLink>
         </li>
-        <li className="cursor-pointer hover:text-primary-400">
+        <li className="cursor-pointer flex justify-center hover:text-primary-400">
           <NavLink to={{hash: '#projects'}} onClick={onClick}>
             <FontAwesomeIcon icon={faBriefcase} />
           </NavLink>
