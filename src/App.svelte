f82bfb555dc8d49a4c400585d001ed863876d7bf
Abdul Rehman <ar618211@gmail.com>
2024-01-11 02:59:51 +0500

    feat: svelte bootstrated and frame for the idea

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import frame from './assets/frame2.png';
+</script>
+
+<main>
+  <div>
+    <img src={frame} alt="frame" />
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: #44475A;
+  }
+  main > div {
+    padding: 1rem;
+  }
+  img {
+    max-height: 100%;
+    max-width: 100%;
+    object-fit: contain;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ]
+  plugins: [svelte()],
 })
