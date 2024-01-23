02ae0d34b5d23e7a9617a88c83f4106a9512702b
Abdul Rehman <ar618211@gmail.com>
2024-01-23 18:41:05 +0500

    changed formats and added new icons

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -1,19 +1,25 @@
-import appleWallet from '../assets/icons/apple-wallet.png';
-import facebook from '../assets/icons/facebook.png';
+import appleWallet from '../assets/icons/apple-wallet.webp';
 import messages from '../assets/icons/messages.png';
 import phone from '../assets/icons/phone.png';
 import photos from '../assets/icons/photos.png';
 import statusBarIcons from '../assets/icons/status-bar-right-icons.png';
 import search from '../assets/icons/magnifyingglass.png';
+import linkedin from '../assets/icons/linkedin.webp';
+import github from '../assets/icons/github.webp';
+import X from '../assets/icons/X.webp';
+import mail from '../assets/icons/mail.webp';
 
 const icons = {
     appleWallet,
-    facebook,
     messages,
     phone,
     photos,
     statusBarIcons,
     search,
+    linkedin,
+    github,
+    X,
+    mail,
 };
 
 export default icons;
