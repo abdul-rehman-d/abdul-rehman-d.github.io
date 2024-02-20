2560c2a6a81fb39dab4143ef088a7babb489a06f
Abdul Rehman <ar618211@gmail.com>
2024-02-20 16:10:12 +0500

    optimized icons

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -2,25 +2,25 @@ import statusBarIcons from '../assets/icons/status-bar-right-icons.png';
 import search from '../assets/icons/magnifyingglass.png';
 
 import appleWallet from '../assets/app-icons/apple-wallet.webp';
-import messages from '../assets/app-icons/messages.png';
-import phone from '../assets/app-icons/phone.png';
-import photos from '../assets/app-icons/photos.png';
+import messages from '../assets/app-icons/messages.webp';
+import phone from '../assets/app-icons/phone.webp';
+import photos from '../assets/app-icons/photos.webp';
 import linkedin from '../assets/app-icons/linkedin.webp';
 import github from '../assets/app-icons/github.webp';
 import X from '../assets/app-icons/X.webp';
 import mail from '../assets/app-icons/mail.webp';
-import medlist from '../assets/app-icons/medlist.png';
+import medlist from '../assets/app-icons/medlist.webp';
 
-import react from '../assets/app-icons/react.png';
-import next from '../assets/app-icons/next.jpg';
-import node from '../assets/app-icons/node.png';
-import express from '../assets/app-icons/express.jpg';
-import typescript from '../assets/app-icons/typescript.png';
-import go from '../assets/app-icons/go.png';
+import react from '../assets/app-icons/react.webp';
+import next from '../assets/app-icons/next.webp';
+import node from '../assets/app-icons/node.webp';
+import express from '../assets/app-icons/express.webp';
+import typescript from '../assets/app-icons/typescript.webp';
+import go from '../assets/app-icons/go.webp';
 import python from '../assets/app-icons/python.webp';
-import git from '../assets/app-icons/git.png';
-import linux from '../assets/app-icons/linux.jpeg';
-import docker from '../assets/app-icons/docker.png';
+import git from '../assets/app-icons/git.webp';
+import linux from '../assets/app-icons/linux.webp';
+import docker from '../assets/app-icons/docker.webp';
 
 const icons = {
     appleWallet,
