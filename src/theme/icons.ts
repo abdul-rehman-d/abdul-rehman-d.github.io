e8746d8a84dd2fcfd1b0e86c5b6013a3acce6eab
Abdul Rehman <ar618211@gmail.com>
2024-02-20 15:33:49 +0500

    rearranging assets

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -1,25 +1,26 @@
-import appleWallet from '../assets/icons/apple-wallet.webp';
-import messages from '../assets/icons/messages.png';
-import phone from '../assets/icons/phone.png';
-import photos from '../assets/icons/photos.png';
 import statusBarIcons from '../assets/icons/status-bar-right-icons.png';
 import search from '../assets/icons/magnifyingglass.png';
-import linkedin from '../assets/icons/linkedin.webp';
-import github from '../assets/icons/github.webp';
-import X from '../assets/icons/X.webp';
-import mail from '../assets/icons/mail.webp';
-import medlist from '../assets/icons/medlist.png';
 
-import react from '../assets/icons/react.png';
-import next from '../assets/icons/next.jpg';
-import node from '../assets/icons/node.png';
-import express from '../assets/icons/express.jpg';
-import typescript from '../assets/icons/typescript.png';
-import go from '../assets/icons/go.png';
-import python from '../assets/icons/python.webp';
-import git from '../assets/icons/git.png';
-import linux from '../assets/icons/linux.jpeg';
-import docker from '../assets/icons/docker.png';
+import appleWallet from '../assets/app-icons/apple-wallet.webp';
+import messages from '../assets/app-icons/messages.png';
+import phone from '../assets/app-icons/phone.png';
+import photos from '../assets/app-icons/photos.png';
+import linkedin from '../assets/app-icons/linkedin.webp';
+import github from '../assets/app-icons/github.webp';
+import X from '../assets/app-icons/X.webp';
+import mail from '../assets/app-icons/mail.webp';
+import medlist from '../assets/app-icons/medlist.png';
+
+import react from '../assets/app-icons/react.png';
+import next from '../assets/app-icons/next.jpg';
+import node from '../assets/app-icons/node.png';
+import express from '../assets/app-icons/express.jpg';
+import typescript from '../assets/app-icons/typescript.png';
+import go from '../assets/app-icons/go.png';
+import python from '../assets/app-icons/python.webp';
+import git from '../assets/app-icons/git.png';
+import linux from '../assets/app-icons/linux.jpeg';
+import docker from '../assets/app-icons/docker.png';
 
 const icons = {
     appleWallet,
