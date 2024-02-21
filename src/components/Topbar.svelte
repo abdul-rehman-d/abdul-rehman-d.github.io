85d86befe705e9606175978fbae741e0f16a0e1b
Abdul Rehman <ar618211@gmail.com>
2024-02-22 03:50:10 +0500

    added icon lib and converted img to svgs

--- src/components/Topbar.svelte
+++ src/components/Topbar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-    import statusBarRightIcons from "../assets/icons/status-bar-right-icons.png";
     import { store } from "../store";
+    import BatteryIcon from 'virtual:icons/heroicons/battery-100-solid';
+    import WifiIcon from 'virtual:icons/material-symbols/wifi-sharp';
+    import BarsIcon from 'virtual:icons/ant-design/signal-filled';
 
     let isAnyOpenOpened: boolean = false;
 
@@ -9,15 +11,15 @@
     });
 </script>
 
-<div class="iPhone__topbar">
-    <p class="iPhone__topbar_time" class:iPhone__topbar_time_light={isAnyOpenOpened}>
+<div class="iPhone__topbar" class:iPhone__topbar_light={isAnyOpenOpened}>
+    <p class="iPhone__topbar_time">
         9:41
     </p>
-    <img
-        src={statusBarRightIcons}
-        alt="Status Bar Right Icons"
-        class="iPhone__topbar_icons"
-    />
+    <div class="iPhone__topbar_icons">
+        <BarsIcon width="1em" height="1em" />
+        <WifiIcon width="1.1em" height="1.1em" />
+        <BatteryIcon width="1.3em" height="1.3em" />
+    </div>
 </div>
 
 <style>
@@ -32,19 +34,21 @@
     }
     .iPhone__topbar > * {
         flex: 1;
+        transition: color 300ms ease-in-out;
+    }
+    .iPhone__topbar_light > * {
+        color: white;
     }
     .iPhone__topbar_time {
         font-size: 14px;
         font-weight: 600;
         text-align: center;
-        transition: color 300ms ease-in-out;
-    }
-    .iPhone__topbar_time_light {
-        color: white;
+        /* transition: color 300ms ease-in-out; */
     }
     .iPhone__topbar_icons {
-        width: 77.5px;
-        height: 13px;
-        object-fit: contain;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: calc(1.2em + 1em + 1.2em + .5em);
     }
 </style>
