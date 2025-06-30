e22d9559913c98d2934fd482f481ee84ae84110e
Abdul Rehman <ar618211@gmail.com>
2025-06-30 16:32:35 +0500

    og image fixed 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: process.env.VERCEL === "1" ? (
+    process.env.VERCEL_ENV === "production" ?
+      `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` :
+      `https://${process.env.VERCEL_BRANCH_URL}`
+  ) : (
+    process.env.NODE_ENV === "production" ?
+    "https://abdul-rehman-d.com" :
+    "http://localhost:4321"
+  ),
 });
